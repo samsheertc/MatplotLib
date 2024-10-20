ada2c7299b49f98871ac14b9d3b8b0e6f3911d0d
samsheer <samsheer.tc@gmail.com>
2024-10-19 22:33:46 -0400

    Commit on 10/18

--- 02-BarCharts/2_finished_code.py
+++ 02-BarCharts/2_finished_code.py
@@ -11,20 +11,21 @@ with open('data.csv', 'r') as csv_file:
     for row in csv_reader:
         language_counter.update(row['LanguagesWorkedWith'].split(';'))
 
+#Option-1
 languages = []
 popularity = []
-
 for item in language_counter.most_common(15):
     languages.append(item[0])
     popularity.append(item[1])
 
-#Alternate
+#Option-2
 #languages, popularity = zip(*language_counter.most_common(15))
+#languages, popularity =  list(languages), list(popularity)
 
 languages.reverse()
 popularity.reverse()
-plt.barh(languages, popularity)
 
+plt.barh(languages, popularity)
 plt.title("Most Popular Languages")
 #plt.ylabel("Programming Languages")
 plt.xlabel("Number if people who use")
--- 02-BarCharts/3_finished_code.py
+++ 02-BarCharts/3_finished_code.py
@@ -15,18 +15,21 @@ language_counter = Counter()
 for response in lang_responses:
     language_counter.update(response.split(';'))
 
+#Option-1
 languages = []
 popularity = []
-
 for item in language_counter.most_common(15):
     languages.append(item[0])
     popularity.append(item[1])
+
+#Option-2
+#languages, popularity = zip(*language_counter.most_common(15))
+#languages, popularity =  list(languages), list(popularity)
 
 languages.reverse()
 popularity.reverse()
 
 plt.barh(languages, popularity)
-
 plt.title("Most Popular Languages")
 # plt.ylabel("Programming Languages")
 plt.xlabel("Number of people who use")
