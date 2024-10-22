7cde478b5288ee8ab7a8b4b34e57a0f586ac9867
samsheer <samsheer.tc@gmail.com>
2024-10-22 13:51:33 -0400

    10/22

---
+++ 09-LiveData/1_Sample_Code.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+x_vals = [0, 1, 2, 3, 4, 5]
+y_vals = [0, 1, 3, 2, 3, 5]
+
+plt.plot(x_vals, y_vals)
+
+plt.tight_layout()
+plt.show()
